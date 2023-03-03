cffc49f4bfd435e087bcd8641039e5e77fbc3911
Hacking Notes <118412415+Hacking-Notes@users.noreply.github.com>
2023-03-03 16:46:28 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -112,4 +112,3 @@ st.markdown('<style>.row-widget.stCheckbox{visibility:hidden;display:none;}</sty
 st.markdown('<style>#tabs-bui3-tab-1 {visibility:hidden;display:none;}</style>', unsafe_allow_html=True)
 st.markdown('<style>#tabs-bui3-tab-2 {visibility:hidden;display:none;}</style>', unsafe_allow_html=True)
 st.markdown('<style>#tabs-bui3-tab-3 {visibility:hidden;display:none;}</style>', unsafe_allow_html=True)
-st.markdown('<style>.css-14xtw13 e8zbici0 {display: none;}</style>', unsafe_allow_html=True)
