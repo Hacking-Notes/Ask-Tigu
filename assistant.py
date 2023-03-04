b49b560e8b87cdeee8d6f1166a1a1508f1a9a021
Hacking Notes <118412415+Hacking-Notes@users.noreply.github.com>
2023-03-03 21:04:25 -0500

    Update assistant.py

--- assistant.py
+++ assistant.py
@@ -128,7 +128,7 @@ def assistant_settings(chat_submitted, col2):
                                                       higher values mean more random.")
 
         
-        settings['specify_sources'] = st.text("https://staratlas.com, https://support.staratlas.com, https://staratlasgame.medium.com/, https://staratlas.club/, https://staratlas.help, https://aephia.com/")
+        settings['specify_sources'] = "https://staratlas.com, https://support.staratlas.com, https://staratlasgame.medium.com, https://staratlas.club, https://staratlas.help, https://aephia.com"
 
         
         with col2.container():
