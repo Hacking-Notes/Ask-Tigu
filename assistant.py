5eea1af1312bdf6e102981942ffc87982895a948
Hacking Notes <118412415+Hacking-Notes@users.noreply.github.com>
2023-03-03 21:01:25 -0500

    Create assistant.py

--- assistant.py
+++ assistant.py
@@ -128,9 +128,8 @@ def assistant_settings(chat_submitted, col2):
                                                       higher values mean more random.")
 
         
-        settings['specify_sources'] = ['https://staratlas.com', 'https://support.staratlas.com', 'https://staratlasgame.medium.com/', 'https://staratlas.club/', 'https://staratlas.help', 'https://aephia.com/']
+        settings['specify_sources'] = st.text("Specify links: https://staratlas.com, https://support.staratlas.com, https://staratlasgame.medium.com/, https://staratlas.club/, https://staratlas.help, https://aephia.com/")
 
-        
         
         with col2.container():
             add_vertical_space(1)
