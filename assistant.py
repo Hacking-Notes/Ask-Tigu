6464d185be3ac73dff32e1d467eb879601d659c4
Hacking Notes <118412415+Hacking-Notes@users.noreply.github.com>
2023-03-04 13:25:03 -0500

    Update assistant.py

--- assistant.py
+++ assistant.py
@@ -127,7 +127,7 @@ def assistant_settings(chat_submitted, col2):
                                                             for the Assistant to source from. \
                                                                 Separate each link with a comma \
                                                                     and space `, `.",
-                                                                    value='https://medium.com/the-hologram, https://staratlas.com, https://support.staratlas.com, https://staratlasgame.medium.com, https://staratlas.club, https://staratlas.help, https://aephia.com') 
+                                                                    value='https://staratlas.com, https://medium.com/the-hologram, https://support.staratlas.com, https://staratlasgame.medium.com, https://staratlas.help/directory') 
         with col2.container():
             add_vertical_space(1)
             
